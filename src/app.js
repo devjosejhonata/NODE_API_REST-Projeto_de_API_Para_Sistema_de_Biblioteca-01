b65f2367ee8ecdb5b069a0be53ae15c1a61b2859
Jose Jhonata <contatojosejhonata@gmail.com>
2024-10-08 15:54:26 -0300

    Criando a rota livros

--- src/app.js
+++ src/app.js
@@ -7,9 +7,25 @@ import express from 'express';
 
 const app = express();
 
+const livros = [ 
+    //dados teste para criar a rota livros, criarei a base de dados em outro lugar
+    {
+        id: 1,
+        título: "O Senhor dos Anéis"
+    },
+    {
+        id: 2,
+        título: "O Hobbit"
+    }
+];
+
 app.get('/', (req, res) => {
     res.status(200).send('Desafio para formação de criação de apis em node');
 });
+
+app.get("/livros", (req, res) => {
+    res.status(200).json(livros);
+  });
 
 export default app;
 
