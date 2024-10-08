7e7b41ad5a435c85ddafa506d0bc80944bafe98a
Jose Jhonata <contatojosejhonata@gmail.com>
2024-10-08 17:50:12 -0300

    Buscando e atualizando livros

--- src/app.js
+++ src/app.js
@@ -6,6 +6,8 @@
 import express from 'express';
 
 const app = express();
+
+// Middleware para permitir que o Express faça o parsing de requisições com corpo em JSON
 app.use(express.json());
 
 const livros = [ 
@@ -20,12 +22,25 @@ const livros = [
     },
 ];
 
+//função para buscar um livro pelo id
+function buscaLivro(id) {
+    return livros.findIndex(livro => {
+        return livro.id === Number(id);
+    });
+}
+
+
 app.get('/', (req, res) => {
     res.status(200).send('Desafio para formação de criação de apis em node');
 });
 
 app.get("/livros", (req, res) => {
     res.status(200).json(livros);
+});
+
+app.get("/livros/:id", (req, res) => {
+    const index = buscaLivro(req.params.id);
+    res.status(200).json(livros[index]);
 });
 
 app.post("/livros", (req, res) => {
